7650028e1fc12649292e84899b9fe76f1fb9f9eb
Katerina Pascoulis <katerina.pascoulis@gmail.com>
2015-10-21 19:21:53 +0100

    testing using geojson instead of polyline

--- public/js/frontend.js
+++ public/js/frontend.js
@@ -1,4 +1,4 @@
-var map = L.map('map').setView([51.509, -0.101], 14);
+var map = L.map('map').setView([102.0, 0.5], 3);
 L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
   attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
   maxZoom: 18,
@@ -6,17 +6,22 @@ L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={
   accessToken: 'pk.eyJ1Ijoic3dhc2hidWNrbGVyIiwiYSI6ImNpZnk2amtsYjAyYTh0NG03ZnAyaDFuaTIifQ.zL7e_N3nod_-P_D9skbAMg'
 }).addTo(map);
 
-L.polyline([[51.504269, -0.113356],[51.513404, -0.088766]], {color:'#66CCCC'}).addTo(map);
+//L.polyline([[51.504269, -0.113356],[51.513404, -0.088766]], {color:'#66CCCC'}).addTo(map);
 
-var marker = L.marker([51.504269, -0.113356]).addTo(map);
+var mygeoJSON = {
+    "type": "LineString",
+    "coordinates": [[51.504269, -0.113356], [51.513404, -0.088766], [104.0, 0.0], [105.0, 1.0]]
+};
 
+L.geoJson(mygeoJSON).addTo(map);
+
+
+//var marker = L.marker([51.504269, -0.113356], {'color': 'red'}).addTo(map);
+//var newmarker = L.marker([51.513404, -0.088766]).addTo(map);
   // icon: L.mapbox.marker.icon({
   //   'marker-color': 'red'
   //   'size':
 
-var t = 0;
-  marker.setLatLng(
- [51.513404, -0.088766]
 
 
 // map.fitBounds(polyline.getBounds());
