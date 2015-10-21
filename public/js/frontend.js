2df97f2d119277d1397e1322c27684211c6cb95c
Josh Phillips <joshuaronan85@yahoo.co.uk>
2015-10-21 17:25:38 +0100

    deleting comments

--- public/js/frontend.js
+++ public/js/frontend.js
@@ -5,28 +5,7 @@ L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={
   id: 'swashbuckler.cify6jkdd02dttcm0s1bknyqr',
   accessToken: 'pk.eyJ1Ijoic3dhc2hidWNrbGVyIiwiYSI6ImNpZnk2amtsYjAyYTh0NG03ZnAyaDFuaTIifQ.zL7e_N3nod_-P_D9skbAMg'
 }).addTo(map);
-// var geojson = { type: 'LineString', coordinates: [[51.504269, -0.113356], [51.513404,-0.088766]] };
-// L.geoJson(geojson).addTo(map);
 
 L.polyline([[51.504269, -0.113356],[51.513404, -0.088766]], {color:'#66CCCC'}).addTo(map);
 
 map.fitBounds(polyline.getBounds());
-
-
-
-
-
-// var myLine = {
-//     "type": "LineString",
-//     "coordinates": [[-100, 40], [-105, 45], [-110, 55]]
-// };
-//
-// var myStyle = {
-//     "color": "#ff7800",
-//     "weight": 5,
-//     "opacity": 0.65
-// };
-//
-// L.geoJson(myLine, {
-//     style: myStyle
-// }).addTo(map);
