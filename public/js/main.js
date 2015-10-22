68dff7ff5a5b7d0e2c7c30f90c3eaf19265f5a9a
katkelemen <kelemenkata@gmail.com>
2015-10-22 13:56:29 +0100

    gettin train information only if data is changed

--- public/js/main.js
+++ public/js/main.js
@@ -8,10 +8,14 @@ socket.on('update from server', function(update){
 
 
 socket.on('update train data', function(update){
-    JSON.parse(update).forEach(function(train){
-      console.log(train.vehicleId);
-      console.log(train.destinationName);
-      console.log(train.timeToStation);
-      console.log(train.currentLocation);
-    });
+      var train1 = update[0];
+      var train2 = update[1];
+
+      //console.log(train.vehicleId);
+      //console.log(train.destinationName);
+      console.log('train1: ' + train1.timeToStation + ' to: ' + train1.destinationName + ' id: ' + train1.vehicleId);
+      console.log('train2: ' + train2.timeToStation + ' to: ' + train2.destinationName + ' id: ' + train2.vehicleId);
+      //console.log(train.currentLocation);
+
+
   });
