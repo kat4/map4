512d915115cabb1e26506cb971afe90b5d151695
katkelemen <kelemenkata@gmail.com>
2015-10-23 10:28:37 +0100

    deploytime

--- app.js
+++ app.js
@@ -5,12 +5,13 @@ console.log('server running on port:1337');
 //io.listen(1337);
 var server = require('http').createServer(Handler);
 var io = require('socket.io')(server);
+var port = process.env.PORT || 1337;
 
 io.on('connection', function(socket){
   socket.emit('update from server', 'win');
   console.log('client connected');
 });
 
-server.listen(1337);
+server.listen(port);
 
 module.exports = io;
